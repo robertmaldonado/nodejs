c41b3999eaef09c1144459b067e5c5f66620e8d1
robert <robertoxt@gmail.com>
2024-10-08 11:18:00 -0500

    agregando codigo

---
+++ var_entorno/index.js
@@ -0,0 +1,14 @@
+//const dotenv = requiere('dotenv')
+//dotenv.config()
+
+//require('dotenv').config()
+
+
+// Importar dotenv con la sintaxis de ESM
+import dotenv from 'dotenv';
+
+// Configurar dotenv para cargar variables del archivo .env
+dotenv.config();
+
+console.log(process.env.NOMBRE_SECRETO)
+console.log(process.env.PUERTO_NUMERO)
