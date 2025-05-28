7dbe30de354ab7f080fb50e6c3b17c6adfe7f74d
robert <robertoxt@gmail.com>
2025-05-28 13:46:31 -0500

    guardando

--- crud_node/src/database.js
+++ crud_node/src/database.js
@@ -1,12 +1,8 @@
 import { createPool } from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config(); // Cargar variables de entorno
 
-/* const pool = createPool({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'Prueba01'
-}); */
+
 
 const pool = createPool({
     host: 'localhost',
@@ -15,6 +11,25 @@ const pool = createPool({
     password: '',
     database: 'taller_admin'
 });
+
+
+/* //hosting
+const pool = createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'epwnhzmc_taller_admin_user',
+    password: 'claveadmin15-',
+    database: 'epwnhzmc_taller_admin'
+}); */
+
+
+/* const pool = createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+}); */
 
 
 export default pool;
